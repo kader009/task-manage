4120f257610233454f80010f3fb15a7c817976f6
kader009 <kadermollarasel000@gmail.com>
2024-08-28 22:34:57 +0600

    home design change

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,39 +1,41 @@
-import FAQSection from "../FAQSection";
-import FeaturesSection from "../FeaturesSection";
-import HeroSection from "../HeroSection";
-import HowItWorksSection from "../HowItWorkSection";
-import PricingSection from "../PricingSection";
-import TestimonialsSection from "../TestimonialSection";
-import NewsletterSection from "../NewsletterSection";
-import BlogSection from "../BlogSection";
+import FAQSection from '../FAQSection';
+import FeaturesSection from '../FeaturesSection';
+import HeroSection from '../HeroSection';
+import HowItWorksSection from '../HowItWorkSection';
+import PricingSection from '../PricingSection';
+import TestimonialsSection from '../TestimonialSection';
+import NewsletterSection from '../NewsletterSection';
+import BlogSection from '../BlogSection';
 
 const Home = () => {
-  return <div>
+  return (
     <div>
-      <HeroSection/>
+      <div>
+        <HeroSection />
+      </div>
+      <div>
+        <FeaturesSection />
+      </div>
+      <div>
+        <HowItWorksSection />
+      </div>
+      <div>
+        <TestimonialsSection />
+      </div>
+      <div>
+        <PricingSection />
+      </div>
+      <div>
+        <FAQSection />
+      </div>
+      <div>
+        <BlogSection />
+      </div>
+      <div>
+        <NewsletterSection />
+      </div>
     </div>
-    <div>
-      <FeaturesSection/>
-    </div>
-    <div>
-      <HowItWorksSection/>
-    </div>
-    <div>
-      <TestimonialsSection/>
-    </div>
-    <div>
-      <PricingSection/>
-    </div>
-    <div>
-      <FAQSection/>
-    </div>
-    <div>
-      <BlogSection/>
-    </div>
-    <div>
-      <NewsletterSection/>
-    </div>
-  </div>;
+  );
 };
 
 export default Home;
