8db0be45cc499d424d4f3c455abab08870735b4e
kader009 <kadermollarasel000@gmail.com>
2024-11-09 15:15:37 +0600

    redux toolkit install

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,16 @@ import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import route from './routes/routes.jsx';
 import AuthProvider from './AuthProvider/AuthProvider.jsx';
+import { Provider } from 'react-redux';
+import { store } from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={route} />
-      <ToastContainer/>
+      <Provider store={store}>
+        <RouterProvider router={route} />
+        <ToastContainer />
+      </Provider>
     </AuthProvider>
   </React.StrictMode>
 );
